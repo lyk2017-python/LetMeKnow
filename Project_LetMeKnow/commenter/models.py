f813f7965f9a84e2a638f89476d63537bd9bca5f
Taner Durkut <tanerdurkut@gmail.com>
2017-07-25 19:55:48 +0300

    Modeller oluşturuldu

--- Project_LetMeKnow/commenter/models.py
+++ Project_LetMeKnow/commenter/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# This object holds firm name and firm id
+class Firm(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    firm = models.ForeignKey(Firm)
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=100)
+    product = models.ForeignKey(Product)
+    message = models.TextField()
+    photo = models.ImageField()
+    rate = models.PositiveSmallIntegerField(default=0)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    #user - will be added
+    #is_published - models.BooleanField(default=False) - comments that approved by admin will be published
