1ac212a5af54a865f5b715bd75a9799e53f24d3f
Taner Durkut <tanerdurkut@gmail.com>
2017-07-27 18:52:30 +0300

    added a field which is populated using function call

--- Project_LetMeKnow/commenter/admin.py
+++ Project_LetMeKnow/commenter/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import Comment, Product, Firm
 
 
+class ProductFirmInline(admin.TabularInline):  #admin sayfasında firmaları listeleyip herhangi birini seçtikten sonra,
+    model = Product#seçilen firmanın ürünlerini listemeyi sağladık.
+    extra = 0
+
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "creation_date", "product"] #
+    list_display = ["id", "title", "creation_date", "product", "rate"] #
     search_fields = ["title", "product__name", "message", "product__firm__name"] # searc attributes
     list_display_links = ['title']  # Linked attributes
     list_filter = ["title", "creation_date", "product"]
@@ -12,10 +17,15 @@ class CommentAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "firm"]  #
+    list_display = ["id", "name", "firm_name"]  #
     search_fields = ["name"]  # search attributes
     list_display_links = ['name']  # Linked attributes
     list_filter = ["name", "firm", ]
+
+    def firm_name(self, object):
+        return object.firm.name
+
+
 
 
 @admin.register(Firm)
@@ -24,3 +34,5 @@ class FirmAdmin(admin.ModelAdmin):
     search_fields = ["name"]  # searc attributes
     list_display_links = ['name']  # Linked attributes
     list_filter = ["name"]
+    inlines = [ProductFirmInline]
+
