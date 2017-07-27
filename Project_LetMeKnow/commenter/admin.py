a9d8b55735748ad10fcb175063957c3c34162ee3
Taner Durkut <tanerdurkut@gmail.com>
2017-07-27 15:33:31 +0300

    little changes about indentation

--- Project_LetMeKnow/commenter/admin.py
+++ Project_LetMeKnow/commenter/admin.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
 from .models import Comment, Product, Firm
-# Register your models here.
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ["id", "title","creation_date","product"] #
-    search_fields = ["title","product__name","message","product__firm__name"] # searc attributes
-    list_display_links = ['title'] # Linked attributes
-    list_filter = ["title","creation_date","product",]
+    list_display = ["id", "title", "creation_date", "product"] #
+    search_fields = ["title", "product__name", "message", "product__firm__name"] # searc attributes
+    list_display_links = ['title']  # Linked attributes
+    list_filter = ["title", "creation_date", "product"]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["id", "name","firm" ]  #
-    search_fields = ["name"]  # searc attributes
+    list_display = ["id", "name", "firm"]  #
+    search_fields = ["name"]  # search attributes
     list_display_links = ['name']  # Linked attributes
     list_filter = ["name", "firm", ]
+
 
 @admin.register(Firm)
 class FirmAdmin(admin.ModelAdmin):
