d9a67f5cacd7debe5f1a73aca4cea21dbed96b6f
Taner Durkut <tanerdurkut@gmail.com>
2017-07-28 15:40:22 +0300

    contact link added

--- Project_LetMeKnow/commenter/urls.py
+++ Project_LetMeKnow/commenter/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from commenter.views import HomePageView, CommentView, SSSView
-
+from commenter.views import HomePageView, CommentView, SSSView, ContactFormView
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^detail/(?P<pk>\d+)$', CommentView.as_view(), name='commentdetails'),
-    url(r'^sss$', SSSView.as_view(), name="faq")
+    url(r'^sss$', SSSView.as_view(), name="faq"),
+    url(r'^contact$', ContactFormView.as_view(), name="contactform")
 ]
 
