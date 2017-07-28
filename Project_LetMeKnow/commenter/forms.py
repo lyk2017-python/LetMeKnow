6704f03ee8935e8da1e1f6eeed29580ac40c371a
Taner Durkut <tanerdurkut@gmail.com>
2017-07-28 18:42:47 +0300

    forms.py file really moved

---
+++ Project_LetMeKnow/commenter/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    email = forms.EmailField()
+    subject = forms.CharField(max_length=50)
+    message = forms.CharField(widget=forms.Textarea(attrs={"row": 2}))
+    ip = forms.GenericIPAddressField(widget=forms.HiddenInput())
