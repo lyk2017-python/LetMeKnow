870dd5d82d7a16fb6a2547b5ffda8d86d3ec4185
Taner Durkut <tanerdurkut@gmail.com>
2017-08-02 15:36:25 +0300

    Header bugfix on shrink

--- Project_LetMeKnow/commenter/static/commenter/js/main.js
+++ Project_LetMeKnow/commenter/static/commenter/js/main.js
@@ -3,14 +3,19 @@ function init() {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 80,
             header = document.querySelector("header");
+            var curr = document.getElementsByClassName("current");
+            var back = document.getElementsByClassName("back");
+            var firstBig = back[0].offsetWidth; //143
+            var smalX = curr[0].offsetWidth; //143
         if (distanceY > shrinkOn) {
             classie.add(header,"smaller");
         } else {
+
             if (classie.has(header,"smaller")) {
                 classie.remove(header,"smaller");
             }
         }
-    });
+        document.getElementsByClassName("back")[0].style.width = document.getElementsByClassName("current")[0].offsetWidth +"px";   });
 }
 window.onload = init();
 
